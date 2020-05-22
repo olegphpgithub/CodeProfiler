9a894501d7278fde0d50151b906181cf87193d27
olegphpgithub <olegphp@yandex.ru>
2020-05-22 15:17:19 +0300

    Add CodeProfiler class;

---
+++ CodeProfiler/CodeProfiler.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <tchar.h>
+#include <Windows.h>
+#include <string>
+
+#define MAX_LOG_BUF_SIZE 2048
+#define USE_CODE_PROFILER
+
+#if defined(USE_CODE_PROFILER)
+
+#   define PRINT_LOG(szFmt, ...) CodeProfiler::PrintLog(szFmt, _T(__FUNCTION__), _T(__FILE__), __LINE__, ##__VA_ARGS__)
+
+#else
+
+#   define PRINT_LOG(szFmt, ...)
+
+#endif
+
+class CodeProfiler
+{
+public:
+    static std::basic_string<TCHAR> m_log;
+    static unsigned int m_startTickCount;
+    static unsigned int m_finishTickCount;
+    static void PrintLog(LPCTSTR szFmt, const TCHAR szFunc[], const TCHAR szFile[], const DWORD dwLine, ...);
+};
+
--- CodeProfiler/main.cpp
+++ CodeProfiler/main.cpp
@@ -3,7 +3,11 @@
 
 #include <iostream>
 
+#include "CodeProfiler.h"
+
 int main()
 {
+    PRINT_LOG(_T("%s %d"), _T("iii"), 789);
+    std::wcout << CodeProfiler::m_log << std::endl;
     std::cout << "Hello World!\n";
 }
